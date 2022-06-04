f1dbc4f24a7b549757805128582e48d03bcab019
jitendra-chauhan <jitendrachauhan527@gmail.com>
2022-06-04 12:55:00 +0530

    create signup

---
+++ src/components/SignUp.js
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "../logo.svg";
+
+export default function SignUp(props) {
+  return (
+    <div className="wrapper">
+    <div className="logo">
+        <img src={logo} alt="logo" />
+    </div>
+    <div className="text-center mt-4 name">
+        chat App
+    </div>
+    <form className="p-3 mt-3" onSubmit={props.handleSubmit} >
+        <div className="form-field d-flex align-items-center">
+            <span className="far fa-user"></span>
+            <input type="text" name="userName" id="userName" onChange={e => props.setUserName(e.target.value)} placeholder="Username" required/>
+        </div>
+        <div className="form-field d-flex align-items-center">
+            <span className="fas fa-key"></span>
+            <input type="password" name="password" id="pwd" onChange={e => props.setPassword(e.target.value)} placeholder="Password" required/>
+        </div>
+            {props.renderErrorMessage("pass")}
+        <div className="form-field d-flex align-items-center">
+            <span className="fas fa-key"></span>
+            <input type="password" name="re-password" id="re-pwd" onChange={e => props.setRePassword(e.target.value)} placeholder="re-Password" required/>
+        </div>
+        <button type="submit" className="btn mt-3">Sign Up</button>
+    </form>
+    <div className="text-center fs-6">
+        <a href="/">Forget password?</a> or <a href="/">Sign in</a>
+    </div>
+</div>
+  );
+}
---
+++ src/pages/SignUp.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "../logo.svg";
+import "../css/Login.css";
+import Navbar from "../components/Navbar";
+import SignUpComp from "../components/SignUp";
+import { URL } from "../common/constants";
+
+function SignUp() {
+  const navigate = useNavigate();
+  const [errorMessages, setErrorMessages] = useState({});
+  const [token, setToken] = useState();
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+  const [rePassword, setRePassword] = useState();
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  async function signUpUser(credentials) {
+    return fetch(`${URL.API}signUp`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    }).then((data) => data.json());
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== rePassword) {
+      return setErrorMessages({
+        name: "pass",
+        message: "password is not match",
+      });
+    } else {
+      setErrorMessages({ name: "pass", message: "" });
+    }
+    const token = await signUpUser({
+      username,
+      password,
+    });
+    console.log(token);
+    setToken(token);
+    navigate("/Chat");
+  };
+
+  return (
+    <>
+      {/* <Navbar title="SignUp Page" /> */}
+      <SignUpComp
+        logo={logo}
+        setUserName={setUserName}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+        setRePassword={setRePassword}
+        renderErrorMessage={renderErrorMessage}
+      />
+    </>
+  );
+}
+
+export default SignUp;
